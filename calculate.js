035a1e7d6f5a56d1079fb0c1cfdd0c2a8ec14fbc
LP5 <noobdoomguy8658@gmail.com>
2020-03-26 12:10:32 +0500

    Visuals.

--- calculate.js
+++ calculate.js
@@ -1,5 +1,5 @@
 function calculate() {
   money = document.getElementById("money").value;
-  lessons = document.getElementById("lessons").value * 340;
+  lessons = document.getElementById("lessons").value * 350;
   document.getElementById("total").value = Number(money) + Number(lessons);
 }
